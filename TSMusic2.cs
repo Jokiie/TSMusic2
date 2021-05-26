0a6430ed6245500e22f3b194d7efefa98495c046
Jokiie <celia.codere63@gmail.com>
2021-05-26 07:49:33 -0500

    Code cleanup

---
+++ TSMusic2.cs
@@ -0,0 +1,133 @@
+using Terraria;
+using Terraria.ModLoader;
+
+
+namespace TSMusic2
+{
+    public class TSMusic2 : Mod
+    {
+        public TSMusic2 Instance;
+
+        public TSMusic2()
+        {
+            Mod = this;
+
+            Instance = this;
+
+            Properties = new ModProperties()
+            {
+                Autoload = true,
+                AutoloadGores = true,
+                AutoloadSounds = true,
+                AutoloadBackgrounds = true
+            };
+        }
+
+        public static Mod TerraStory { get; set; }
+
+        public static TSMusic2 Mod { get; set; }
+
+        public override void UpdateMusic(ref int music, ref MusicPriority priority)
+        {
+
+            if (Main.myPlayer != -1 && !Main.gameMenu)
+            {
+                if (!Main.dayTime
+                && Main.player[Main.myPlayer].ZoneOverworldHeight
+                && !Main.player[Main.myPlayer].ZoneJungle
+                && !Main.player[Main.myPlayer].ZoneSnow
+                && !Main.player[Main.myPlayer].ZoneCrimson
+                && !Main.player[Main.myPlayer].ZoneCorrupt)
+                //&& !Main.player[Main.myPlayer].ZoneHoly
+                {
+                    music = (this).GetSoundSlot(SoundType.Music, "Sounds/Music/HenesysShadow");
+                    priority = MusicPriority.BiomeLow;
+                }
+                if (Main.player[Main.myPlayer].ZoneHoly)
+                {
+                    music = (this).GetSoundSlot(SoundType.Music, "Sounds/Music/DreamHallow");
+                    priority = MusicPriority.BiomeLow;
+                }
+                if (Main.player[Main.myPlayer].ZoneCorrupt)
+                {
+                    music = (this).GetSoundSlot(SoundType.Music, "Sounds/Music/Laby");
+                    priority = MusicPriority.BiomeHigh;
+                }
+                if (Main.player[Main.myPlayer].ZoneCrimson)
+                {
+                    music = (this).GetSoundSlot(SoundType.Music, "Sounds/Music/CrimsonwoodCrimson");
+                    priority = MusicPriority.BiomeHigh;
+                }
+                if (Main.player[Main.myPlayer].ZoneDirtLayerHeight
+                && !Main.player[Main.myPlayer].ZoneJungle
+                && !Main.player[Main.myPlayer].ZoneSnow
+                && !Main.player[Main.myPlayer].ZoneCrimson
+                && !Main.player[Main.myPlayer].ZoneCorrupt
+                && !Main.player[Main.myPlayer].ZoneHoly
+                && !Main.player[Main.myPlayer].ZoneDesert)
+                {
+                    music = (this).GetSoundSlot(SoundType.Music, "Sounds/Music/UnderGround");
+                    priority = MusicPriority.Environment;
+                }
+                if (Main.player[Main.myPlayer].ZoneRockLayerHeight
+                && !Main.player[Main.myPlayer].ZoneJungle
+                && !Main.player[Main.myPlayer].ZoneSnow
+                && !Main.player[Main.myPlayer].ZoneCrimson
+                && !Main.player[Main.myPlayer].ZoneCorrupt
+                && !Main.player[Main.myPlayer].ZoneHoly
+                && !Main.player[Main.myPlayer].ZoneDesert)
+                {
+                    music = (this).GetSoundSlot(SoundType.Music, "Sounds/Music/AbandonnedMines");
+                    priority = MusicPriority.Environment;
+                }
+                if (Main.player[Main.myPlayer].ZoneSandstorm
+                && Main.player[Main.myPlayer].ZoneDesert)
+                {
+                    music = (this).GetSoundSlot(SoundType.Music, "Sounds/Music/PerionSandstorm");
+                    priority = MusicPriority.Environment;
+                }
+                if (Main.player[Main.myPlayer].ZoneBeach)
+                {
+                    music = (this).GetSoundSlot(SoundType.Music, "Sounds/Music/BlueWorld");
+                    priority = MusicPriority.BiomeLow;
+                }
+                if (Main.player[Main.myPlayer].ZoneRain
+                && Main.player[Main.myPlayer].ZoneOverworldHeight
+                && !Main.player[Main.myPlayer].ZoneJungle
+                && !Main.player[Main.myPlayer].ZoneCorrupt
+                && !Main.player[Main.myPlayer].ZoneCrimson
+                && !Main.player[Main.myPlayer].ZoneBeach
+                && !Main.player[Main.myPlayer].ZoneHoly
+                && !Main.player[Main.myPlayer].ZoneDesert
+                && !Main.player[Main.myPlayer].ZoneSkyHeight
+                && !Main.player[Main.myPlayer].ZoneSnow)
+                {
+                    music = (this).GetSoundSlot(SoundType.Music, "Sounds/Music/MissingYouRain");
+                    priority = MusicPriority.Event;
+                }
+                if (Main.dayTime
+                && Main.player[Main.myPlayer].ZoneOverworldHeight
+                && !Main.player[Main.myPlayer].ZoneDesert
+                && !Main.player[Main.myPlayer].ZoneJungle
+                && !Main.player[Main.myPlayer].ZoneSnow
+                && !Main.player[Main.myPlayer].ZoneCrimson
+                && !Main.player[Main.myPlayer].ZoneCorrupt
+                && !Main.player[Main.myPlayer].ZoneHoly)
+                {
+                    music = (this).GetSoundSlot(SoundType.Music, "Sounds/Music/HenesysFields");
+                    priority = MusicPriority.BiomeLow;
+                }
+                if (Main.player[Main.myPlayer].ZoneJungle)
+                {
+                    music = (this).GetSoundSlot(SoundType.Music, "Sounds/Music/JungleBook");
+                    priority = MusicPriority.Environment;
+                }
+                if (Main.player[Main.myPlayer].ZoneDesert)
+                {
+                    music = (this).GetSoundSlot(SoundType.Music, "Sounds/Music/SunsetDesert");
+                    priority = MusicPriority.Environment;
+                }
+            }
+        }
+    }
+}
